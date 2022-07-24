cf32b9fbad05f0bfae9fc95cc16dc3da8b826f4d
Akash Das <akashd595@gmail.com>
2022-07-24 22:38:47 +0530

    ADDED: in linked list

---
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.linkedlist;
+
+public class MyLinkedList<K> {
+    public INode<K> head;
+    public INode<K> tail;
+
+    public MyLinkedList() {}
+
+    public void add(INode<K> node) {
+        INode<K> tempNode = node;
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.seNext(tempNode);
+            tail = tempNode;
+        }
+    }
+    public void printList(){
+        System.out.println("MyNode "+head);
+    }
+    public static void main(String[] args) {
+        MyLinkedList<Integer> listNode = new MyLinkedList<>();
+        MyNode<Integer> node = new MyNode<>(70);
+        MyNode<Integer> node2 = new MyNode<>(56);
+        MyNode<Integer> node3 = new MyNode<>(30);
+        listNode.add(node);
+        listNode.add(node2);
+        listNode.add(node3);
+
+        listNode.printList();
+    }
+}
