4ce02cca1fd13d12dfeac036d20bf08147100c58
Akash Das <akashd595@gmail.com>
2022-07-24 22:48:41 +0530

    ADDED: adding middle

--- src/com/bridgelabz/linkedlist/MyLinkedList.java
+++ src/com/bridgelabz/linkedlist/MyLinkedList.java
@@ -17,18 +17,46 @@ public class MyLinkedList<K> {
             tail = tempNode;
         }
     }
+    public void addFirst(INode<K> node) {
+        INode<K> tempNode = node;
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else{
+            tempNode.seNext(head);
+            head = tempNode;
+        }
+    }
+    public void addMiddle(INode<K> node) {
+        INode<K> newNode = node;
+        INode<K> oldNode = node;
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else{
+            oldNode= head.getNext();
+            head.seNext(newNode);
+            newNode.seNext(oldNode);
+        }
+    }
     public void printList(){
         System.out.println("MyNode "+head);
     }
     public static void main(String[] args) {
         MyLinkedList<Integer> listNode = new MyLinkedList<>();
         MyNode<Integer> node = new MyNode<>(56);
-        MyNode<Integer> node2 = new MyNode<>(30);
-        MyNode<Integer> node3 = new MyNode<>(70);
+        MyNode<Integer> node2 = new MyNode<>(70);
+        MyNode<Integer> node3 = new MyNode<>(30);
         listNode.add(node);
-        listNode.add(node2);
-        listNode.add(node3);
+        listNode.printList();
 
+        listNode.add(node2);
+        listNode.printList();
+
+        listNode.addMiddle(node3);
         listNode.printList();
     }
 }
